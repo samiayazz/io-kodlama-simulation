477ee9ac0c5d1d964b2b2b4d464fa9143e9b057a
samiayazz <samiayazdv@gmail.com>
2022-10-22 09:48:58 +0300

    fix: educator entity encapsulation

--- src/entity/Educator.java
+++ src/entity/Educator.java
@@ -1,7 +1,51 @@
 package entity;
 
+import java.util.List;
+
 public class Educator {
-    public int id;
-    public String firstName;
-    public String lastName;
+    public Educator() {
+    }
+
+    public Educator(String firstName, String lastName) {
+        setFirstName(firstName);
+        setLastName(lastName);
+    }
+
+    private int id = generateRandomId();
+    private List<Course> courses;
+    private String firstName;
+    private String lastName;
+
+    private int generateRandomId() {
+        double id = Math.random() * 100;
+        return (int) id;
+    }
+
+    public int getId() {
+        return id;
+    }
+
+    public List<Course> getCourses() {
+        return courses;
+    }
+
+    public void setCourses(List<Course> courses) {
+        this.courses = courses;
+    }
+
+    public String getFirstName() {
+        return firstName;
+    }
+
+    public void setFirstName(String firstName) {
+        this.firstName = firstName;
+    }
+
+    public String getLastName() {
+        return lastName;
+    }
+
+    public void setLastName(String lastName) {
+        this.lastName = lastName;
+    }
 }
