45d11db49d7a4650c61c100c3613403d6caeae14
samiayazz <samiayazdv@gmail.com>
2022-10-22 08:48:18 +0300

    feat: ui main method and configuration for orm and log type

--- src/ui/Main.java
+++ src/ui/Main.java
@@ -1,7 +1,12 @@
 package ui;
 
+import core.config.Config;
+import core.enums.LogType;
+import core.enums.ORM;
+
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+        Config.orm = ORM.JDBC;
+        Config.logType = LogType.FILE;
     }
 }
