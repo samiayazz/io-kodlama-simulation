fd6d50d528732a34a406e71000c574bf3ed5b2fb
samiayazz <samiayazdv@gmail.com>
2022-10-24 21:05:05 +0300

    feat: base course data access object

---
+++ src/dao/virtual/BaseCourseDao.java
@@ -0,0 +1,16 @@
+package dao.virtual;
+
+import entity.Category;
+import entity.Course;
+
+import java.util.List;
+
+public interface BaseCourseDao {
+    List<Course> get();
+
+    void add(Course course);
+
+    void update(int oldCourseId, Course newCourse);
+
+    void delete(int id);
+}
