ef5b330cb0a5d7e3fc27424444362a3b5722fa42
samiayazz <samiayazdv@gmail.com>
2022-10-24 21:26:48 +0300

    feat: jdbc category data access object

---
+++ src/dao/concrete/jdbc/JDBCCategoryDao.java
@@ -0,0 +1,33 @@
+package dao.concrete.jdbc;
+
+import dao.virtual.BaseCategoryDao;
+import entity.Category;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class JDBCCategoryDao implements BaseCategoryDao {
+    @Override
+    public List<Category> get() {
+        //JDBC Operations
+        return new ArrayList<Category>();
+    }
+
+    @Override
+    public void add(Category category) {
+        //JDBC Operations
+        System.out.println("JDBC: \"" + category.getName() + "\" adlı kategori oluşturuldu!");
+    }
+
+    @Override
+    public void update(int oldCategoryId, Category newCategory) {
+        //JDBC Operations
+        System.out.println("JDBC: \"" + newCategory.getName() + "\" adlı kategori güncellendi!");
+    }
+
+    @Override
+    public void delete(int id) {
+        //JDBC Operations
+        System.out.println("JDBC: Kategori silindi!");
+    }
+}
