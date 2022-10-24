ab5ef959aa8d29e6a59a0a1f55522daf31ef9f4f
samiayazz <samiayazdv@gmail.com>
2022-10-24 21:32:32 +0300

    feat: jdbc course data access object

---
+++ src/dao/concrete/jdbc/JDBCCourseDao.java
@@ -0,0 +1,33 @@
+package dao.concrete.jdbc;
+
+import dao.virtual.BaseCourseDao;
+import entity.Course;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class JDBCCourseDao implements BaseCourseDao {
+    @Override
+    public List<Course> get() {
+        //JDBC Operations
+        return new ArrayList<Course>();
+    }
+
+    @Override
+    public void add(Course course) {
+        //JDBC Operations
+        System.out.println("JDBC: \"" + course.getName() + "\" adlı kurs oluşturuldu!");
+    }
+
+    @Override
+    public void update(int oldCourseId, Course newCourse) {
+        //JDBC Operations
+        System.out.println("JDBC: \"" + newCourse.getName() + "\" adlı kurs oluşturuldu!");
+    }
+
+    @Override
+    public void delete(int id) {
+        //JDBC Operations
+        System.out.println("JDBC: Kurs silindi!");
+    }
+}
