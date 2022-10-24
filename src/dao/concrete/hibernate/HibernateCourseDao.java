be3c2353578f4ea4df4a601dbe93c344efed0704
samiayazz <samiayazdv@gmail.com>
2022-10-24 21:33:26 +0300

    feat: hibernate course data access object

---
+++ src/dao/concrete/hibernate/HibernateCourseDao.java
@@ -0,0 +1,33 @@
+package dao.concrete.hibernate;
+
+import dao.virtual.BaseCourseDao;
+import entity.Course;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class HibernateCourseDao implements BaseCourseDao {
+    @Override
+    public List<Course> get() {
+        //Hibernate Operations
+        return new ArrayList<Course>();
+    }
+
+    @Override
+    public void add(Course course) {
+        //Hibernate Operations
+        System.out.println("Hibernate: \"" + course.getName() + "\" adlı kurs oluşturuldu!");
+    }
+
+    @Override
+    public void update(int oldCourseId, Course newCourse) {
+        //Hibernate Operations
+        System.out.println("Hibernate: \"" + newCourse.getName() + "\" adlı kurs oluşturuldu!");
+    }
+
+    @Override
+    public void delete(int id) {
+        //Hibernate Operations
+        System.out.println("Hibernate: Kurs silindi!");
+    }
+}
